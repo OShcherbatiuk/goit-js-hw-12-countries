1242e73d5ddbd011adfe5328ebd094bc2cf5d61d
Oleksii Shcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2021-03-18 16:39:05 +0200

    upd

---
+++ goit-js-hw-12-countries/src/js/fetchCountries.js
@@ -0,0 +1,6 @@
+const BASE_URL = 'https://restcountries.eu/rest/v2/name'
+
+export default function fetchCountryByName(searchQuery) {
+    return fetch(`${BASE_URL}/${searchQuery}`).then(responce => { return responce.json(); })
+};
+
